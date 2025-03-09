bd3870f32f47efc8c8c60022bf913666fe201de4
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2025-03-09 19:41:38 +0200

    basic

--- investment_calc.go
+++ investment_calc.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 )
 
-func main() {
+//func sdmain() {
 	var revenue, expenses, taxRate float64
+
 	fmt.Print("Enter revenue:")
 	fmt.Scan(&revenue)
 	fmt.Print("Enter expenses:")
 	fmt.Scan(&expenses)
 	fmt.Print("Enter tax rate:")
 	fmt.Scan(&taxRate)
+
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / revenue
-	fmt.Println("EBT: ", ebt)
-	fmt.Println("Profit: ", profit)
-	fmt.Println("Ratio: ", ratio)
+	//ratio := ebt / revenue
+
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", profit)
+
+	fmt.Println(formattedFV)
+	// fmt.Printf("EBT: %.1f\n", ebt)
+	// fmt.Println("EBT: ", ebt)
+	// fmt.Println("Profit: ", profit)
+	// fmt.Println("Ratio: ", ratio)
 }
--- second.go
+++ second.go
@@ -1,2 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide numbers")
+		return
+	}
+	var total, nInts, nFloats int
+	invalid := make([]string, 0)
+	for _, k := range arguments[1:] {
+		_, err := strconv.Atoi(k)
+		if err != nil {
+			invalid = append(invalid, k)
+			continue
+		}
+		total++
+		nInts++
+	}
+	fmt.Println("Total: ", total, " Ints: ", nInts, " Floats: ", nFloats)
+	if len(invalid) > 0 {
+		fmt.Printf("Invalid inputs: %v\n", len(invalid))
+		for _, s := range invalid {
+			fmt.Println(s)
+		}
+	}
+}
