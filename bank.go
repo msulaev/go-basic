e02c942db70905e8a2133eea8e0dd790f6ec4552
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2025-03-10 21:32:46 +0200

    added changes

--- bank.go
+++ bank.go
@@ -8,52 +8,51 @@ var f = fmt.Println
 
 func main() {
 	accountBalance := 1000.0
-	for {
-		fmt.Println("Welcome to the Bank of Go!")
-		fmt.Println("What would you like to do?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
+	fmt.Println("Welcome to the Bank of Go!")
+	fmt.Println("What would you like to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
 
-		var choice int
-		fmt.Print("Your Choice: ")
-		fmt.Scan(&choice)
+	var choice int
+	fmt.Print("Your Choice: ")
+	fmt.Scan(&choice)
 
-		if choice == 1 {
-			f("Your balance is", accountBalance)
-		} else if choice == 2 {
-			f("How much would you like to deposit?")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
+	switch choice {
+	case 1:
+		f("Your balance is", accountBalance)
+	case 2:
+		f("How much would you like to deposit?")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
 
-			if depositAmount <= 0 {
-				f("Invalid deposit amount!")
-				continue
-			}
-
-			accountBalance += depositAmount
-			f("Balance after deposit is", accountBalance)
-		} else if choice == 3 {
-			f("How much would you like to withdraw?")
-			var withdrawAmount float64
-			fmt.Scan(&withdrawAmount)
-
-			if withdrawAmount <= 0 {
-				f("Invalid withdrawal amount!")
-				continue
-			}
-
-			if withdrawAmount > accountBalance {
-				f("Not enough money!")
-				return
-			}
-			accountBalance -= withdrawAmount
-			f("Balance after withdrawal is", accountBalance)
-		} else {
-			f("Bye!")
-			break
+		if depositAmount <= 0 {
+			f("Invalid deposit amount!")
+			continue
 		}
+
+		accountBalance += depositAmount
+		f("Balance after deposit is", accountBalance)
+	case 3:
+		f("How much would you like to withdraw?")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+		if withdrawAmount <= 0 {
+			f("Invalid withdrawal amount!")
+			continue
+		}
+
+		if withdrawAmount > accountBalance {
+			f("Not enough money!")
+			return
+		}
+		accountBalance -= withdrawAmount
+		f("Balance after withdrawal is", accountBalance)
+	default:
+		f("Bye!")
+		break
 	}
 	f("Thanks for using our bank!")
 }
